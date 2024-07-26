8850b7aba05ddd9c644d7e027ee7cb58a5156e22
InshutiSouvede <isouvede2@gmail.com>
2024-07-26 16:23:20 +0200

    style todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import './App.css'
 import TodoItem from './components/TodoItem'
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className='text-[100px] text-center text-gray-300'>todos</h1>
-      <TodoItem name="" id="" description=""/>
+      <TodoItem name="" id={nanoid()} description="I am Here"/>
     </>
   )
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,8 @@
 export default function TodoItem(props){
     return (
-        <>
+        <div className="flex gap-5 text-gray-500 text-2xl">
+        <input type="checkbox" id={props.id} name={props.name?props.name:"todoItem"} value={props.description} />
         <label htmlFor={props.id}>{props.description}</label>
-        <input type="checkbox" id={props.id} name={props.name?props.name:"todoItem"} />
-        </>
+        </div>
     )
 }
